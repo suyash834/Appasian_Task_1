8c7a5d6544620529be8c38ca41ddd29b41c19ead
“suyash834git config --global user.email “suyash.dixir834@gmail.com <suyash.dixit834@gmail.com>
2025-10-25 22:57:56 +0530

    add gitignore file

--- obj/Debug/net8.0/TaskManager.Api.AssemblyInfo.cs
+++ obj/Debug/net8.0/TaskManager.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManager.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43abe2e33e951d3fd8541dbe2d526b409659a7e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManager.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManager.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
