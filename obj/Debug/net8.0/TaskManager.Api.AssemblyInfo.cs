73224b1d66c332d78a843377992ada14dc4473e0
“suyash834git config --global user.email “suyash.dixir834@gmail.com <suyash.dixit834@gmail.com>
2025-10-26 02:15:09 +0530

    feat : DEPLOYMENT FIX

--- obj/Debug/net8.0/TaskManager.Api.AssemblyInfo.cs
+++ obj/Debug/net8.0/TaskManager.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManager.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+637fd8e6da25c04704d5002615d64d3cca48a0e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f1676130cef883fd966dcb60dbece00ce2d9763")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManager.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManager.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
