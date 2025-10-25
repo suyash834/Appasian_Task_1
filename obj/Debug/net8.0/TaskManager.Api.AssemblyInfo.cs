4baad36514d95dc008d3615f27e199538b2b12e2
“suyash834git config --global user.email “suyash.dixir834@gmail.com <suyash.dixit834@gmail.com>
2025-10-26 01:22:51 +0530

    feat : add dockerfile and dockerignore

--- obj/Debug/net8.0/TaskManager.Api.AssemblyInfo.cs
+++ obj/Debug/net8.0/TaskManager.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManager.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c7a5d6544620529be8c38ca41ddd29b41c19ead")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+637fd8e6da25c04704d5002615d64d3cca48a0e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManager.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManager.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
